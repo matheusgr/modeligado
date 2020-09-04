29948a67f5a57c1729729455ea861109e061aad0
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2020-09-04 16:44:31 -0300

    Simplified save process.

--- modeligado_storage.js
+++ modeligado_storage.js
@@ -1,29 +1,22 @@
 import {exampleData} from './example_data.js'
 
-function load(buttons) {
+function load() {
     let text = null
     if (localStorage.getItem("uml")) {
         text = localStorage.getItem("uml")
-        buttons.save.disabled = true
     } else {
-        buttons.clear.disabled = true
-        clear(buttons)
         text = exampleData()
     }
     return text
 }
 
-function clear(buttons) {
-    buttons.load.disabled = true
-    buttons.clear.disabled = true
-    localStorage.clear();
+function loadExample() {
+    return exampleData()
 }
 
-function save(text, buttons) {
-    buttons.save.disabled = true
-    buttons.load.disabled = false
-    buttons.clear.disabled = false
+function save(text) {
     localStorage.setItem('uml', text);
 }
 
-export {load, clear, save}
+
+export {load, loadExample, save}
