bccafb2bc0d3a07d0d0422c441d76eef241259cc
Matheus Gaudencio do Rêgo <matheusgr@users.noreply.github.com>
2021-10-01 10:41:16 -0300

    Refactor convert to smaller functions.

--- convert.js
+++ convert.js
@@ -1,9 +1,19 @@
-function extractParameters (parameters) {
-  return parameters?.map(param => processType(param.type) + ' ' + param.name).join(', ') || ''
-}
-
-function processType (t) {
-  return (t === 'str') ? 'String' : t
+function extractParameters(parameters) {
+    if (!parameters) {
+        return ""
+    }
+    let first = true
+    let paramResult = ""
+    for (let param of parameters) {
+        let paramName = param.name
+        let paramType = processType(param.type)
+        if (!first) {
+            paramResult += ', '
+        }
+        first = false
+        paramResult += paramType + " " + paramName
+    }
+    return paramResult
 }
 
 function extractProperty (prop, excludeType = false) {
@@ -12,6 +22,7 @@ function extractProperty (prop, excludeType = false) {
             prop.name
 }
 
+<<<<<<< HEAD
 function extractMethods (methods, constructors, prop, isInterface) {
   const parameters = prop.parameters
   if (processType(prop.type)) { // Method -- has return
@@ -23,6 +34,90 @@ function extractMethods (methods, constructors, prop, isInterface) {
                         ' {\n\n    }\n')
   }
 }
+=======
+function extractProperty(prop, excludeType=false) {
+    let propVisibility = prop.visibility + " "
+    let propType = "";
+    if (!excludeType) {
+        propType = processType(prop.type) + " "
+    }
+    let propName = prop.name
+    return (propVisibility + propType + propName)
+}
+
+function extractMethods(methods, constructors, prop, isInterface) {
+    let parameters = prop.parameters
+    if (processType(prop.type)) {  // Method -- has return
+        let methodSign = extractProperty(prop) + "(" +  extractParameters(parameters) + ")"
+        if (isInterface) {
+            methodSign += ";\n"
+        } else {
+            methodSign += " {\n\n    }\n"
+        }
+        methods.push(methodSign)
+    } else { // Constructor
+        constructors.push(extractProperty(prop, true) + "(" +  extractParameters(parameters) + ") {\n\n    }\n")
+    }
+}
+
+function extractRelation(prop, implement) {
+    let extend = ""
+    let propTo = prop.to.split(" ", 1)
+    let propRelationship = prop.relationship
+    if (propRelationship === "generalization") {
+        extend += " extends " + propTo
+    }
+    if (propRelationship === "generalizationInterface") {
+        implement.push(propTo)
+    }
+    return extend
+}
+
+function extractClassSignature(qualifier) {
+    let classSign = "public "
+        if (qualifier === "<Interface>") {
+            classSign += "interface "
+        } else if (qualifier === "<Abstract>") {
+            classSign += "abstract class "
+        } else {
+            classSign += "class "
+        }
+    return classSign;
+}
+
+function generateSourceFile(package_, classSign, props, constructors, methods) {
+    let result = ""
+    
+    if (package_) {
+        result += "package " + package_ + ";\n\n"
+    }
+
+    result += classSign + " {\n"
+    
+    if (props.length > 0) {
+        result += "\n    " + props.join("\n    ") + "\n"
+    }
+
+    if (constructors.length > 0) {
+        result += "\n    " + constructors.join("\n    ")
+    }
+
+    if (methods.length > 0) {
+        result += "\n    " + methods.join("\n    ")
+    }
+    
+    result += "\n}"
+
+    return result
+}
+
+function process(parseData, package_) {
+    let results = {}
+    for (let class_ of parseData) {
+        let context = class_.context
+        let nameArray = context.name.split(" ")
+        let fileName = nameArray[0]
+>>>>>>> Refactor convert to smaller functions.
 
 function extractRelation (prop, implement) {
   let extend = ''
@@ -42,6 +137,7 @@ function extractClassSignature (qualifier) {
   return 'public ' + qualifierMap[qualifier]
 }
 
+<<<<<<< HEAD
 function generateSourceFile (package_, classSignature, props, constructors, methods) {
   return (package_ ? 'package ' + package_ + ';\n\n' : '') +
                 classSignature + ' {\n' +
@@ -80,6 +176,35 @@ function process (parseData, package_) {
 
     if (implement.length > 0) {
       classSignature += ' implements ' + implement.join(', ')
+=======
+        let classSign = extractClassSignature(qualifier)
+        let isInterface = (qualifier === "<Interface>")
+        
+        classSign += fileName
+        
+        let props = []
+        let methods = []
+        let constructors = []
+        let implement = []
+
+        for (let prop of context.properties) {
+            props.push(extractProperty(prop) +  ";")
+        }
+
+        for (let prop of context.methods) {
+            extractMethods(methods, constructors, prop, isInterface)
+        }
+
+        for (let prop of class_.relations) {
+            classSign += extractRelation(prop, implement)
+        }
+        
+        if (implement.length > 0) {
+            classSign += " implements " + implement.join(", ")
+        }
+
+        results[fileName] = generateSourceFile(package_, classSign, props, constructors, methods)
+>>>>>>> Refactor convert to smaller functions.
     }
 
     results[fileName] = generateSourceFile(package_, classSignature, props, constructors, methods)
